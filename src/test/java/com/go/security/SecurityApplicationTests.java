fbaabd6ca8634b9c0db680b5c4702462eda3b3f1
gus <ggortiz09@gmail.com>
2020-12-08 23:15:27 -0300

    scurity in memmory

---
+++ src/test/java/com/go/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.go.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
